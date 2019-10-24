a1f92818d9a7f5692134884291d0e84f2ff9fce5
Marcin Trocha <cinekmar21@gmail.com>
2019-10-24 18:46:05 +0200

    init

---
+++ WebAPI.Model/Entities/Bet.MapConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace WebAPI.Model.Entities
+{
+    public class BetMapConfiguration : IEntityTypeConfiguration<Bet>
+    {
+        public void Configure(EntityTypeBuilder<Bet> builder)
+        {
+            builder.ToTable("bet");
+            builder.HasKey(c => c.ID);
+            builder.Property(c => c.Name).HasMaxLength(255);
+            builder.Property(c => c.Type);
+            builder.Property(c => c.Score);
+            builder.Property(c => c.Name);
+        }
+    }
+}
